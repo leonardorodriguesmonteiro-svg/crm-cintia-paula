e499e1f0836f11712b17ee7f737adb0e8e763413
leonardorodriguesmonteiro-svg <leonardorodriguesmonteiro@gmail.com>
2026-08-21 21:54:57 -0300

    feat: integra formalizacao v2 na pagina de orcamentos

--- app/orcamentos/page.tsx
+++ app/orcamentos/page.tsx
@@ -1,10 +1,14 @@
 import { PageLayout } from '@/components/PageLayout'
+import { FormalizacaoComercialV2Panel } from '@/components/comercial/FormalizacaoComercialV2Panel'
 import { OrcamentosPage } from '@/components/comercial/OrcamentosPage'
 
 export default function Page() {
   return (
     <PageLayout>
-      <OrcamentosPage />
+      <div className="space-y-6">
+        <FormalizacaoComercialV2Panel />
+        <OrcamentosPage />
+      </div>
     </PageLayout>
   )
 }
